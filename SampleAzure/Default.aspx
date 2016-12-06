173251dd37a998ef848cc30cecd0e9bc62eefb04
sadewa <sadewa@SUNNYDEWANGAN>
2016-12-06 13:01:43 +0530

    Add project files.

---
+++ SampleAzure/Default.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="SampleAzure.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table width="50%">
+                <tr>
+                    <td>Resource Group :
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtRG" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>WebApp :
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtWebappName" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>App Service Plan :
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtASP" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Location :
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtLocation" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <asp:Button ID="btnSubmit" runat="server" Text="Create" OnClick="btnSubmit_Click" />
+                    </td>
+
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ SampleAzure/Default.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Management.Automation;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace SampleAzure
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+
+
+            ExecutePowershell();
+        }
+
+        protected void ExecutePowershell()
+        {
+            string WebAppName = txtWebappName.Text;
+            string ResourceGroup = txtRG.Text;
+            string AppServicePlan = txtASP.Text;
+            string Location = txtLocation.Text;
+
+            using (PowerShell PowerShellInstance = PowerShell.Create())
+            {
+                try
+                {
+                    PowerShellInstance.AddCommand("Login-AzureRmAccount");
+                    PowerShellInstance.Invoke();
+                 //   PowerShellInstance.AddCommand("Select-AzureSubscription -SubscriptionName 'Microsoft Azure Internal Consumption' ");
+                    //   PowerShellInstance.AddCommand("Get-AzureRmSubscription -SubscriptionName 'Microsoft Azure Internal Consumption' | Select-AzureRmSubscription");
+               //     PowerShellInstance.Invoke();
+                    PowerShellInstance.AddCommand("New-AzureRmResourceGroup -ResourceGroupName '" + ResourceGroup + "' -Location '" + Location + "'");
+                    PowerShellInstance.Invoke();
+                    Response.Write("<H1>Webapp Successfully Created</H1>");
+                }
+                catch (Exception ex)
+                {
+                    Response.Write(ex.Message);
+                }
+            }
+        }
+    }
+}
